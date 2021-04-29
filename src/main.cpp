bfdca351111275b4c0217f4bb14f634dc3fbffc6
Felix Sarnthein <safelix@student.ethz.ch>
2021-04-29 17:54:50 +0200

    dumping of HDR images: - non-breaking change - LDR is still default - open .pfm with tev viewer

--- src/main.cpp
+++ src/main.cpp
@@ -466,7 +466,7 @@ static hit sphere_trace(vec origin, vec dir) {
 
 // }}}
 
-static void dump_image(std::ostream& out, int width, int height, const float* pixels) {
+static void dump_image_ldr(std::ostream& out, int width, int height, const float* pixels) {
     out << "P6\n" << width << " " << height << "\n255\n";
 
     for (int j = 0; j < height; j++) {
@@ -476,6 +476,26 @@ static void dump_image(std::ostream& out, int width, int height, const float* pi
                 out << channel;
             }
         }
+    }
+}
+
+static void dump_image_hdr(std::ostream& out, int width, int height, const float* pixels) {
+    out << "PF\n" << width << " " << height << "\n-1.0\n";
+
+    for (int j = height-1; j > -1; j--) {
+        for (int i = 0; i < width; i++) {
+            out.write((char *) &pixels[3 * (width * j + i)], 3 * sizeof(float));
+        }
+    }   
+}
+
+static void dump_image(std::ostream& out, int width, int height, const float* pixels, bool hdr=false){
+    if (hdr) {
+        // store in binary .pfm format
+        dump_image_hdr(out, width, height, pixels);
+    } else {
+        // store in binary .ppm format
+        dump_image_ldr(out, width, height, pixels);
     }
 }
 
