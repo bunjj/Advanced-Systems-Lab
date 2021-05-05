c807aebeabc6275843c49f9519dec6dadf445ded
Irfan <ibunjaku@student.ethz.ch>
2021-05-05 09:13:37 +0200

    adjustments to use the script with comand line inputs

---
+++ benchmark/creat_scene.py
@@ -0,0 +1,62 @@
+from helper import *
+from random import random as rand
+import json
+import sys
+
+shapes = ["plane", "box", "sphere", "cone" , "torus", "octahedron"]
+
+if len(sys.argv) != 4:
+    sys.exit("not the right number of arguments")
+elif sys.argv[1] not in shapes:
+    sys.exit("first argument not a shape!")
+
+shape = sys.argv[1]
+n = int(sys.argv[2])
+path = sys.argv[3]
+
+#create scene
+scene = {}
+scene["camera"] = camera()
+scene["pointlight"] = pointlight()
+objects = []
+#load plane
+o = object(kind = "plane")
+o["params"] = plane()
+objects.append(o)
+
+for i in range(n):
+    pos = [0,0,0]
+    pos[0] = rand() * 16 - 8
+    pos[1] = rand() * 6 - 3
+    pos[2] = rand() * 5 + 12.5
+    color = [rand(), rand(), rand()]
+    rotation = [30*rand(),30*rand(),30*rand()]
+    reflection = rand()
+
+    o = object(kind = shape, position= pos, color= color, reflection=reflection, rotation=rotation)
+
+    if shape == "box":
+        extent = [rand(), rand(), rand()]
+        o["params"] = box(extent)
+    elif shape == "sphere":
+        radius = rand() * 0.75
+        o["params"] = sphere(radius)
+    elif shape == "cone":
+        o["params"] = cone([rand(), rand(), rand()])
+    elif shape == "torus":
+        r2 = rand()
+        r1 = rand() + r2
+        o["params"] = torus(r1, r2)
+    elif shape == "octahedron":
+        o["params"] = octahedron(rand())
+    else:
+        sys.exit("Wrong shape format")
+    objects.append(o)
+
+
+scene["objects"] =objects
+
+with open(path, 'w') as outfile:
+    json.dump(scene, outfile, indent= 4)
+#print(json.dumps(scene, indent = 4))
+
