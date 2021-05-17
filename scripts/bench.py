7b45c93d0c34e00240ac0b33e47315af34ba76f2
patrick96 <p.ziegler96@gmail.com>
2021-05-17 21:17:38 +0200

    Run all benchmarks using the same input scenes
    Now produces a json file with datapoints that we can parse to generate
    gnuplot data files

--- scripts/bench.py
+++ scripts/bench.py
@@ -3,8 +3,8 @@
 from pathlib import Path
 import subprocess
 import sys
-from collections import OrderedDict
 import tempfile
+import json
 
 script_dir = Path(__file__).parent
 build_exec = script_dir / "build.sh"
@@ -14,80 +14,60 @@ shape_scene_creator = script_dir / ".." / "benchmark" / "create_scene.py"
 all_shapes_scene_creator = script_dir / ".." / \
     "benchmark" / "create_scene_all_shapes.py"
 
-fields = ["Width", "Height", "Flops", "Cycles", "Microseconds"]
+# Fields to collect from the program output
+fields = ["Flops", "Cycles", "Microseconds"]
 
-x_base = 0
-x_end = 0
-x_step = 0
+datapoints = []
+
+shape_scenes = {}
+
+impls = ["ref", "opt0", "opt1", "opt3"]
+shapes = ["all", "box", "sphere", "cone", "torus", "octahedron"]
+bench_types = ["size"] + shapes
+# flags = [
+#     "-O2",
+#     "-O3 -fno-tree-vectorize",
+#     "-O3",
+#     "-Ofast",
+#     "-Ofast -march=native"]
+
+flags = ["-Ofast"]
 
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def get_range():
+
+def get_range(bench_type):
+    if bench_type == "size":
+        x_base = 200
+        x_end = 4200
+        x_step = 200
+    elif bench_type in ["all", "box", "sphere", "cone", "torus", "octahedron"]:
+        x_base = 10
+        x_end = 110
+        x_step = 10
+    else:
+        raise ValueError(bench_type)
+
     return range(x_base, x_end, x_step)
 
-def run_single(impl, scene, width, height):
-    p = subprocess.run([str(run_exec), impl, str(scene), str(
-        width), str(height)], check=True, capture_output=True)
 
-    values = {}
+def gen_shape_scenes(temp_dir):
 
-    for line in p.stderr.decode().split('\n'):
-        if ': ' in line:
-            key, value = line.split(': ')
+    for shape in shapes:
+        for num_shapes in get_range(shape):
+            fname = f"{shape}-{num_shapes}.json"
+            scene_path = temp_dir / fname
 
-            if key in fields:
-                # eprint("{}={}".format(key, value))
-                values[key] = value
+            if shape == "all":
+                subprocess.run([str(all_shapes_scene_creator), str(
+                    num_shapes // 5), str(scene_path)], check=True)
+            else:
+                subprocess.run([str(shape_scene_creator), shape, str(
+                    num_shapes), str(scene_path)], check=True)
 
-    return values
-
-
-def run_size_benchmark(impl, name, results: dict):
-    """
-    Runs the benchmark on the given implementation across multiple image sizes.
-
-    results is a dictionary mapping from widths to a dictionary containing the measurements
-
-    Results from this benchmark run are added to the dictionary for the widths under 'name'
-    """
-
-    for width in get_range():
-        eprint("{}x{}".format(width, width))
-        r = results.setdefault(width, {})
-        r[name] = run_single(impl, input_scene, width, width)
-
-
-def run_shape_benchmark(impl, temp_dir, shape, name, results):
-    """
-    Runs the benchmark on the given implementation on a scene with the given shape with increasing number of shapes
-
-    results is a dictionary mapping from number of shapes to a dictionary containing the measurements
-
-    Results from this benchmark run are added to the dictionary for the number of shapes under 'name'
-    """
-
-    # Fixed size
-    width = 1920
-    height = 1080
-
-    for num_shapes in get_range():
-        eprint(f"{num_shapes} x {shape} @ {width}x{height}")
-        fname = f"{impl}-{shape}-{num_shapes}-{width}x{height}-{name}.json"
-        scene_path = temp_dir / fname
-
-        if shape == "all":
-            subprocess.run([str(all_shapes_scene_creator), str(
-                num_shapes // 5), str(scene_path)], check=True)
-        else:
-            subprocess.run([str(shape_scene_creator), shape, str(
-                num_shapes), str(scene_path)], check=True)
-
-        r = results.setdefault(num_shapes, {})
-        r[name] = run_single(impl, scene_path, width, height)
-
-    return results
+            shape_scenes.setdefault(shape, {})[num_shapes] = scene_path
 
 
 def build(flag, instrument):
@@ -98,106 +78,105 @@ def build(flag, instrument):
     else:
         cmd.append("-DINSTRUMENT=OFF")
 
-    eprint(cmd)
+    eprint(" ".join(cmd))
 
     p = subprocess.run(cmd, capture_output=True)
 
     if p.returncode != 0:
         eprint(p.stdout.decode())
-        eprint(p.stderr.decode())
+        eprint(f"\033[31;1m{e.stderr.decode()}\033[0m")
         raise subprocess.CalledProcessError(
             p.returncode, p.args, p.stdout, p.stderr)
 
 
-def print_line(line, quotes=False):
-    for l in line:
-        if quotes:
-            print('\t"{}"'.format(l), end='')
-        else:
-            print('\t{}'.format(l), end='')
+def run_single(impl, scene, width, height, datapoint_base: dict):
+    p = subprocess.run([str(run_exec), impl, str(scene), str(
+        width), str(height)], check=True, capture_output=True)
 
-    print('')
+    datapoint = datapoint_base.copy()
+    datapoint["scene"] = str(scene)
+    datapoint["width"] = width
+    datapoint["height"] = height
+
+    for line in p.stderr.decode().split('\n'):
+        if ': ' in line:
+            key, value = line.split(': ')
+
+            if key in fields:
+                datapoint[key] = value
+
+    datapoints.append(datapoint)
 
 
-def print_results(results, x_name, flags):
-    header = [x_name, "flops"]
+def run_size_benchmark(impl, datapoint_base: dict):
+    """
+    Runs the benchmark on the given implementation across multiple image sizes.
+    """
 
-    for flag in flags:
-        header.extend([flag +
-                       " - seconds", flag +
-                       " - cycles", flag +
-                       " - flops/cycle"])
+    for width in get_range("size"):
+        eprint("{}x{}".format(width, width))
+        datapoint_base2 = datapoint_base.copy()
+        run_single(impl, input_scene, width, width, datapoint_base2)
 
-    print_line(header, True)
 
-    for x_val, measurements in results.items():
-        num_flops = int(measurements["Flops"]["Flops"])
+def run_shape_benchmark(impl, shape, datapoint_base: dict):
+    """
+    Runs the benchmark on the given implementation on a scene with the given shape with increasing number of shapes
+    """
 
-        line = [x_val, num_flops]
+    # Fixed size
+    width = 1920
+    height = 1080
 
-        for flag in flags:
-            r = measurements[flag]
-            usec = float(r["Microseconds"])
-            cycles = float(r["Cycles"])
-            line.extend([usec / 1e6, cycles, num_flops / cycles])
+    for num_shapes in get_range(shape):
+        eprint(f"{num_shapes} x {shape} @ {width}x{height}")
+        datapoint_base2 = datapoint_base.copy()
+        datapoint_base2["num_shapes"] = num_shapes
 
-        print_line(line, False)
+        scene_path = shape_scenes[shape][num_shapes]
 
+        run_single(impl, scene_path, width, height, datapoint_base2)
+
+
+def run_with_flags(flag, do_instrument, datapoint_base: dict):
+    build(flag, do_instrument)
+    for impl in impls:
+        for bench_type in bench_types:
+            print_header(f"Running benchmark type '{bench_type}' for implementation '{impl}' with flags '{flag}'")
+
+            datapoint_base2 = datapoint_base.copy()
+            datapoint_base2.update({
+                "flag": flag,
+                "impl": impl,
+                "type": bench_type})
+
+            if bench_type == "size":
+                run_size_benchmark(impl, datapoint_base)
+            else:
+                run_shape_benchmark(impl, bench_type, datapoint_base)
+
+def print_header(name):
+    eprint(f"\033[32;1m{name}\033[0m")
 
 def main(temp_dir):
 
-    flags = [
-        "-O2",
-        "-O3 -fno-tree-vectorize",
-        "-O3",
-        "-Ofast",
-        "-Ofast -march=native"]
+    print_header("Generate Shape Scenes")
+    gen_shape_scenes(temp_dir)
 
-    if len(sys.argv) < 6:
-        eprint(f"Usage: {sys.argv[0]} <impl> <type> <x-base> <x-end> <x-step>")
-        eprint("\t<type> must be one of the following: 'size', 'all', 'box', 'sphere', 'cone', 'torus', 'octahedron'")
-        sys.exit(1)
-
-    impl = sys.argv[1]
-    benchmark_type = sys.argv[2]
-    global x_base
-    global x_end
-    global x_step
-    x_base = int(sys.argv[3])
-    x_end = int(sys.argv[4])
-    x_step = int(sys.argv[5])
-
-    if benchmark_type == "size":
-        flops_runner = lambda results: run_size_benchmark(
-            impl, "Flops", results)
-        benchmark_runner = lambda flag, results: run_size_benchmark(
-            impl, flag, results)
-        x_name = "width/height"
-    else:
-        flops_runner = lambda results: run_shape_benchmark(
-            impl, temp_dir, benchmark_type, "Flops", results)
-        benchmark_runner = lambda flag, results: run_shape_benchmark(
-            impl, temp_dir, benchmark_type, flag, results)
-        x_name = f"number of shapes ({benchmark_type})"
-
-    eprint("Flops Counter")
+    print_header("Flops Counter")
     # Count flops first
-    build("", True)
-
-    results = OrderedDict()
-    flops_runner(results)
+    run_with_flags("", True, {"flops": True})
 
     for flag in flags:
-        build(flag, False)
-        benchmark_runner(flag, results)
+        run_with_flags(flag, False, {"flops": False})
 
-    print_results(results, x_name, flags)
+    print(json.dumps(datapoints, indent=4))
 
 if __name__ == "__main__":
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
             main(Path(temp_dir))
     except subprocess.CalledProcessError as e:
-        print(e.stdout.decode())
-        print(f"\033[31;1m{e.stderr.decode()}\033[0m")
+        eprint(e.stdout.decode())
+        eprint(f"\033[31;1m{e.stderr.decode()}\033[0m")
         raise e
